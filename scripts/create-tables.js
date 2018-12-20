fcbc48c902dcc9f289584f5a30e8a22a4f57f2b4
‘Michael <pickup.patterns@gmail.com>
2018-12-20 13:39:19 -0800

    updating backend tables for collecting api object info

--- scripts/create-tables.js
+++ scripts/create-tables.js
@@ -18,7 +18,7 @@ client.query(`
   CREATE TABLE IF NOT EXISTS album (
     id SERIAL PRIMARY KEY,
     album VARCHAR(256) NOT NULL,
-    rating INTEGER,
+    year INTEGER,
     artist_id INTEGER REFERENCES artist(id)
   )`)
   .then(
